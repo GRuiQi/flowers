8bf31dc1ce369f48e046de3eef2ee7ae8df61eb0
GRuiQi <2654344573@qq.com>
2021-03-28 07:54:05 +0800

    使用LinUI评分组件快速实现分数预览

--- components/post/index.js
+++ components/post/index.js
@@ -19,6 +19,15 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    onGoToDetail(event){
+      const pid = event.currentTarget.dataset.postId;
+      wx.navigateTo({
+        // 在url传递查询参数
+        url: '/pages/post-detail/post-detail?pid='+pid
+  
+      })
+    },
+  
 
   }
 })
